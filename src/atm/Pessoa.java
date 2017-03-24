ac7062f48ad11bb1915769561067616c4c165c65
JBP1996 <jbp.1996@hotmail.com>
2017-03-24 15:27:48 +0000

    ATM -> Aula prof dario

---
+++ src/atm/Pessoa.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atm;
+
+/**
+ *
+ * @author JBP1996 Atec
+ */
+public class Pessoa {
+    private String nome;
+    
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
